072ce1448d09d6489ad163021a43ae8b03e002a4
Ahmed Ibrahim <a.ibrahim@itqansystems.com>
2022-01-30 22:38:15 +0000

    init wasm

--- src/WebUI/Pages/Error.cshtml.cs
+++ src/WebUI/Pages/Error.cshtml.cs
@@ -1,26 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-
-namespace WebUI.Pages;
-
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-public class ErrorModel : PageModel
+namespace WebUI.Pages
 {
-    private readonly ILogger<ErrorModel> _logger;
-
-    public ErrorModel(ILogger<ErrorModel> logger)
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    [IgnoreAntiforgeryToken]
+    public class ErrorModel : PageModel
     {
-        _logger = logger;
-    }
+        public string? RequestId { get; set; }
 
-    public string? RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        private readonly ILogger<ErrorModel> _logger;
 
-    public void OnGet()
-    {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
     }
 }
--- src/WebUI/Startup.cs
+++ src/WebUI/Startup.cs
@@ -1,123 +1,104 @@
-using Application;
-using Application.Common.Interfaces;
-using Infrastructure;
-using Infrastructure.Persistence;
-using WebUI.Filters;
-using WebUI.Services;
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Mvc;
-using NSwag;
-using NSwag.Generation.Processors.Security;
+//using Application;
+//using Application.Common.Interfaces;
+//using Infrastructure;
+//using Infrastructure.Persistence;
+//using WebUI.Filters;
+//using WebUI.Services;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace WebUI;
+//namespace WebUI;
 
-public class Startup
-{
-    public Startup(IConfiguration configuration)
-    {
-        Configuration = configuration;
-    }
+//public class Startup
+//{
+//    public Startup(IConfiguration configuration)
+//    {
+//        Configuration = configuration;
+//    }
 
-    public IConfiguration Configuration { get; }
+//    public IConfiguration Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-        services.AddApplication();
-        services.AddInfrastructure(Configuration);
+//    // This method gets called by the runtime. Use this method to add services to the container.
+//    public void ConfigureServices(IServiceCollection services)
+//    {
+//        services.AddApplication();
+//        services.AddInfrastructure(Configuration);
 
-        services.AddDatabaseDeveloperPageExceptionFilter();
+//        services.AddDatabaseDeveloperPageExceptionFilter();
 
-        services.AddSingleton<ICurrentUserService, CurrentUserService>();
+//        services.AddSingleton<ICurrentUserService, CurrentUserService>();
 
-        services.AddHttpContextAccessor();
+//        services.AddHttpContextAccessor();
 
-        services.AddHealthChecks()
-            .AddDbContextCheck<ApplicationDbContext>();
+//        services.AddHealthChecks()
+//            .AddDbContextCheck<ApplicationDbContext>();
 
-        services.AddControllersWithViews(options =>
-            options.Filters.Add<ApiExceptionFilterAttribute>())
-                .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
+//        services.AddControllersWithViews(options =>
+//            options.Filters.Add<ApiExceptionFilterAttribute>())
+//                .AddFluentValidation(x => x.AutomaticValidationEnabled = false);
 
-        services.AddRazorPages();
+//        services.AddRazorPages();
 
-        // Customise default API behaviour
-        services.Configure<ApiBehaviorOptions>(options => 
-            options.SuppressModelStateInvalidFilter = true);
+//        // Customise default API behaviour
+//        services.Configure<ApiBehaviorOptions>(options => 
+//            options.SuppressModelStateInvalidFilter = true);
 
-        // In production, the Angular files will be served from this directory
-        services.AddSpaStaticFiles(configuration => 
-            configuration.RootPath = "ClientApp/dist");
 
-        services.AddOpenApiDocument(configure =>
-        {
-            configure.Title = "WASMClean API";
-            configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
-            {
-                Type = OpenApiSecuritySchemeType.ApiKey,
-                Name = "Authorization",
-                In = OpenApiSecurityApiKeyLocation.Header,
-                Description = "Type into the textbox: Bearer {your JWT token}."
-            });
+//        services.AddOpenApiDocument(configure =>
+//        {
+//            configure.Title = "WASMClean API";
+//            configure.AddSecurity("JWT", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+//            {
+//                Type = OpenApiSecuritySchemeType.ApiKey,
+//                Name = "Authorization",
+//                In = OpenApiSecurityApiKeyLocation.Header,
+//                Description = "Type into the textbox: Bearer {your JWT token}."
+//            });
 
-            configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
-        });
-    }
+//            configure.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+//        });
+//    }
 
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseMigrationsEndPoint();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
-        }
+//    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//    {
+//        if (env.IsDevelopment())
+//        {
+//            app.UseDeveloperExceptionPage();
+//            app.UseMigrationsEndPoint();
+//        }
+//        else
+//        {
+//            app.UseExceptionHandler("/Error");
+//            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//            app.UseHsts();
+//        }
 
-        app.UseHealthChecks("/health");
-        app.UseHttpsRedirection();
-        app.UseStaticFiles();
-        if (!env.IsDevelopment())
-        {
-            app.UseSpaStaticFiles();
-        }
+//        app.UseHealthChecks("/health");
+//        app.UseHttpsRedirection();
+//        app.UseStaticFiles();
+//        if (!env.IsDevelopment())
+//        {
 
-        app.UseSwaggerUi3(settings =>
-        {
-            settings.Path = "/api";
-            settings.DocumentPath = "/api/specification.json";
-        });
+//        }
 
-        app.UseRouting();
+//        app.UseSwaggerUi3(settings =>
+//        {
+//            settings.Path = "/api";
+//            settings.DocumentPath = "/api/specification.json";
+//        });
 
-        app.UseAuthentication();
-        app.UseIdentityServer();
-        app.UseAuthorization();
-        app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllerRoute(
-                name: "default",
-                pattern: "{controller}/{action=Index}/{id?}");
-            endpoints.MapRazorPages();
-        });
+//        app.UseRouting();
 
-        app.UseSpa(spa =>
-        {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+//        app.UseAuthentication();
+//        app.UseIdentityServer();
+//        app.UseAuthorization();
+//        app.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapControllerRoute(
+//                name: "default",
+//                pattern: "{controller}/{action=Index}/{id?}");
+//            endpoints.MapRazorPages();
+//        });
 
-                spa.Options.SourcePath = "ClientApp";
-
-            if (env.IsDevelopment())
-            {
-                    //spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer(Configuration["SpaBaseUrl"] ?? "http://localhost:4200");
-            }
-        });
-    }
-}
+//    }
+//}
